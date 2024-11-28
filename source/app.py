ff25fe8ddb16bb974db89b6e269a5044936e8a94
Dang Vue <dang.vue0@gmail.com>
2024-11-28 17:06:42 -0600

    update dataframe

--- source/app.py
+++ source/app.py
@@ -1,38 +1,46 @@
 from flask import Flask, jsonify, g
 import pandas as pd
 import os
-
+from flask_caching import Cache
+import numpy as np
 
 
 app = Flask(__name__)
 
-data = {'Name': ['John', 'Mary', 'Peter'], 'Age': [20, 25, 30]}
-df = pd.DataFrame(data)
+# Configure caching
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
+# data = {'Name': ['John', 'Mary', 'Peter'], 'Age': [20, 25, 30]}
+# df = pd.DataFrame(data)
+# directory = 'csv_files'
+# if not os.path.exists(directory):
+#     os.makedirs(directory)
     
 # df.to_csv('csv_files/data1.csv', index=False)
 # print(df)
 
-data = {'Name': ['DDDD', 'AAAA', 'BBBB'], 'Age': [22, 33, 44]}
-df_next = pd.DataFrame(data)
+# data = {'Name': ['DDDD', 'AAAA', 'BBBB'], 'Age': [22, 33, 44]}
+# df_next = pd.DataFrame(data)
 # df_next.to_csv('csv_files/data2.csv', index=False)
 # print(df_next)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    
     return jsonify(f"This is App.")
 
-@app.route('/df', methods=['GET'])
-def show_df():
-    global df
-    return jsonify(df.to_dict(orient='records'))
 
-@app.route('/df/get_update', methods=['GET'])
-def updat_df():
-    global df
-    df = df_next.copy()
+
+
+@cache.cached(timeout=5)
+def generate_random_df():
+    df = pd.DataFrame(np.random.randint(0, 100, size=(500, 10)), columns=list('ABCDEFGHIJ'))
+    return df
+
+@app.route('/df', methods=['GET'])
+def get_df():
+    df = generate_random_df()
+    
     return jsonify(df.to_dict(orient='records'))
    
    
