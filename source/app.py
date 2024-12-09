e5bd53976e6783a377786faa3085a388c41cbe46
Dang Vue <dang.vue0@gmail.com>
2024-12-09 15:48:32 -0600

    apscheduler not working

--- source/app.py
+++ source/app.py
@@ -5,13 +5,38 @@ import pandas as pd
 import numpy as np
 from flask_apscheduler import APScheduler
 import logging
- 
-logger = logging.getLogger(__name__)
-logger.setLevel = logging.DEBUG
+
+# Configure logging before creating the Flask app
+logging.basicConfig(
+    filename='app.log',  # Optional: Log to a file
+    level=logging.DEBUG,  # Set the desired logging level
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # Optional: Customize the log format
+)
+
+class Config:
+    """App configuration."""
+
+    SCHEDULER_API_ENABLED = True
+    
+scheduler = APScheduler()
+
+@scheduler.task("interval", id="do_job_1", seconds=30, misfire_grace_time=900)
+def job1():
+    """Sample job 1."""
+    print("Job 1 executed")
+    
 
 app = Flask(__name__)
+app.config.from_object(Config())
 
-scheduler = APScheduler()
+# Configure the logger
+
+# logging.basicConfig(filename='app.log', level=logging.DEBUG)
+# logger.info("Flask starting...")
+
+
+
+    
 scheduler.init_app(app)
 scheduler.start()
 
@@ -20,13 +45,10 @@ cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
 def clear_cache():
     cache.clear()
+    
+def hello():
+    print('TEST')
 
-    
-@scheduler.task('cron', id='do_job_1', hour=21, minute=43, misfire_grace_time=900)
-def job1():
-    logger.info("APSCHEDULER RUNNING")
-    cache.clear()
-    
 
 @app.route('/', methods=['GET'])
 def index():
